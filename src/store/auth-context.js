a211fe2c23dcc439492bdcb71b1846ad81635a86
nguye374 <nguye374@sheridancollege.ca>
2021-10-08 02:58:40 -0400

    Oct 8 3rd

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -7,17 +7,29 @@ const AuthContext = React.createContext({
     logout: () => {}
 });
 
+const calculateRemainingTime = (expirationTime) => {
+    const currentTime = new Date().getTime();
+    const adjExpirationTime = new Date(expirationTime).getTime();
+    const remainingTime = adjExpirationTime - currentTime;
+    return remainingTime;
+}
 export const AuthContextProvider = (props) => {
-    const [token, setToken] = useState(null);
+    const initialtoken = localStorage.getItem('token');
+    const [token, setToken] = useState(initialtoken);
 
     const userIsLoggedIn = !!token;
 
-    const loginHandler = (token) => {
-        setToken(token);
-    };
-
     const logoutHandler = () => {
         setToken(null);
+        localStorage.removeItem('token');
+    };
+
+    const loginHandler = (token, expirationTime) => {
+        setToken(token);
+        localStorage.setItem('token', token);
+        const remainingTime = calculateRemainingTime(expirationTime);
+        setTimeout(logoutHandler, remainingTime);
+        
     };
 
     const contextValue = {
