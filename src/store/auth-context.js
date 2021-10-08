72dbafe6e3a40715fdd1414d74c991bb3cf93335
nguye374 <nguye374@sheridancollege.ca>
2021-10-08 03:19:41 -0400

    Oct 8 5th

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 let logoutTimer;
 
@@ -45,7 +45,7 @@ export const AuthContextProvider = (props) => {
 
     const userIsLoggedIn = !!token;
 
-    const logoutHandler = () => {
+    const logoutHandler = useCallback(() => {
         setToken(null);
         localStorage.removeItem('token');
         localStorage.removeItem('expirationTime');
@@ -53,7 +53,7 @@ export const AuthContextProvider = (props) => {
         if (logoutTimer) {
             clearTimeout(logoutTimer)
         }
-    };
+    },[]);
 
     const loginHandler = (token, expirationTime) => {
         setToken(token);
@@ -69,7 +69,7 @@ export const AuthContextProvider = (props) => {
             console.log(tokenData.duration);
             logoutTimer = setTimeout(logoutHandler, tokenData.duration);
         }
-    }, [tokenData])
+    }, [tokenData, logoutHandler])
 
     const contextValue = {
         token,
